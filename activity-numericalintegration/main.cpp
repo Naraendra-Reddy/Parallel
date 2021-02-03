c153649727057eb1801298feba22093466a16627
Naraendra-Reddy <d.naraendrareddy@gmail.com>
2021-02-02 20:22:40 -0500

    from VM,

--- activity-numericalintegration/main.cpp
+++ activity-numericalintegration/main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <chrono>
+using namespace std::chrono;
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,6 +17,7 @@ float f4(float x, int intensity);
 }
 #endif
 
+
   
 int main (int argc, char* argv[]) {
 
@@ -23,6 +25,33 @@ int main (int argc, char* argv[]) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
     return -1;
   }
-  
+
+ auto start = std::chrono::system_clock::now();
+  int functionid = atoi(argv[1]);
+  float a= strtof(argv[2],NULL);
+  float b=strtof(argv[3],NULL); 
+  int n= atoi(argv[4]);
+  int intensity=atoi(argv[5]);
+  float temp= (b-a)/n;
+  float sum=0;float f=0;
+  for(int i=0;i<n;i++)
+    {
+      float x=(a+(i+.5)*temp);
+      if(functionid==1)
+	f=f1(x,intensity);
+      else if(functionid == 2)
+	f=f2(x,intensity);
+      else if(functionid ==3)
+	f=f3(x,intensity);
+      else if(functionid ==4)
+	f=f4(x,intensity);
+
+      sum=sum+f;
+    }
+  sum=sum*temp;
+  std::cout<<sum<<std::endl;
+ auto end=std::chrono::system_clock::now();
+ auto  elapsed=duration_cast<seconds>(end-start);
+ std::cerr<<elapsed.count()<<std::endl;	  
   return 0;
 }
