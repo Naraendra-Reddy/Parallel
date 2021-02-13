9e2764fff30fce4a52b084ecb05aa7b301998414
Naraendra-Reddy <d.naraendrareddy@gmail.com>
2021-02-13 17:48:07 -0500

    The actual hello files

--- activity-hello/hello.cpp
+++ activity-hello/hello.cpp
@@ -1,57 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<iostream>
 #include<unistd.h>
-#include<errno.h>
-#include<sys/types.h>
-#include<netdb.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
 
-void checkHostName(int hostname)
+using namespace std;
+
+
+char *getname(void)
 {
-  if (hostname==-1)
-    { perror("gethostname");
-  exit(1);
-    }
+  static char name[256];
+  gethostname(name,255);
+  return name;
 }
-void checkIPbuffer(char *IPbuffer)
-{
-  if(NULL == IPbuffer)
-    {
-      perror("inet_ntoa");
-      exit(1);
-    }
-}
-void checkHostEntry(struct hostent * hostentry)
-{
-  if (hostentry ==NULL)
-    {
-      perror("gethostbyname");
-      exit(1);
-    }
+  int main()
+{ 
+  system("clear");
+  char *hostname;
+   hostname= getname();
+   std::cout<<"\n"<<hostname<<"\n";
+ return 0;
 }
 
-int main()
-{
-  char hostbuffer[256];
-  char *IPbuffer;
-  struct hostent *host_entry;
-  int hostname;
-
-  hostname=gethostname(hostbuffer, sizeof(hostbuffer));
-  checkHostName(hostname);
-
-  host_entry=gethostbyname(hostbuffer);
-   checkHostEntry(host_entry);
-
-  IPbuffer=inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
-  printf("Hostbame: %s\n", hostbuffer);
-  printf("Host IP: %s", IPbuffer);
-  return 0;
-  
-  
-
-
-
-} 
