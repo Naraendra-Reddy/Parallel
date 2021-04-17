3e73b552087fe9d62f32da9856b8373a809abc2c
Naraendra-Reddy <49789233+Naraendra-Reddy@users.noreply.github.com>
2021-04-17 18:38:18 -0400

    Update MyHashtable.hpp

--- activity_coarse/fine_grain/MyHashtable.hpp
+++ activity_coarse/fine_grain/MyHashtable.hpp
@@ -1,8 +1,9 @@
 #ifndef _MY_HASHTABLE_H
 #define _MY_HASHTABLE_H
+#include <mutex>
+#include <shared_mutex>
 #include <functional>
 #include <iostream>
-#include <mutex>
 #include <vector>
 
 template<class K, class V>
@@ -28,7 +29,9 @@ protected:
   int count;
   double loadFactor;
   std::vector<Node<K,V>*> table;
-  std::mutex mut[224];
+  std::mutex m[224];
+  //mutable std::shared_time_mutex mutex_;
+  // std::condition_variable_any cond;
   bool done = false;
 
   struct hashtable_iter : public dict_iter {
@@ -112,17 +115,23 @@ public:
   virtual V get(const K& key) const {
     std::size_t index = std::hash<K>{}(key) % this->capacity;
     index = index < 0 ? index + this->capacity : index;
-   
+    int kth_lock = index/224;
+    // m[kth_lock].lock();
+    // mu.lock();
+    // cond.wait(mu, [&]() {return (done)||
+    std::shared_lock<std::shared_time_mutex> lock(m[kth_lock]);
     const Node<K,V>* node = this->table[index];
 
     while (node != nullptr) {
       if (node->key == key){
-	
+	//m[kth_lock].unlock();
+	//	mu.unlock();
 	      return node->value;
       }
       node = node->next;
     }
-    
+    //m[kth_lock].unlock();
+    // mu.unlock();
     return V();
   }
 
@@ -134,16 +143,18 @@ public:
   virtual void set(const K& key, const V& value) {
     std::size_t index = std::hash<K>{}(key) % this->capacity;
     index = index < 0 ? index + this->capacity : index;
-    int loc_n = index/224;
-    mut[loc_n].lock(); 
-    //std::unique_lock<std::shared_time_mutex> lock(mutex_);
+    int kth_lock = index%224;
+    //m[kth_lock].lock(); 
+    std::unique_lock<std::shared_time_mutex> lock(m[kth_lock]);
+    //std::unique_lock<mutex>lck(m[kth_lock]);
     Node<K,V>* node = this->table[index];
     
     while (node != nullptr) {
       if (node->key == key) {
 	      node->value = value;
-	     
-	      mut[loc_n].unlock();
+	      //m[kth_lock].unlock();
+	      //cond.notify_one();
+	      // mu.unlock();
 	      return;
       }
       node = node->next;
@@ -153,7 +164,9 @@ public:
     node = new Node<K,V>(key, value);
     node->next = this->table[index];
     this->table[index] = node;
-    mut[loc_n].unlock();
+    //m[kth_lock].unlock();
+    //cond.notify_one();
+    //mu.unlock();
     this->count++;
     if (((double)this->count)/this->capacity > this->loadFactor) {
       this->resize(this->capacity * 2);
